daf3e721622c421090f6dd44ef211fdfca3264bd
jac-mainwaring <72376933+jac-mainwaring@users.noreply.github.com>
2023-05-25 14:53:29 +0100

    nav mesh ai implementation

---
+++ Assets/PlayerNav.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class PlayerNav : MonoBehaviour
+{
+    [SerializeField] private Transform movePositionTransform;
+
+    private NavMeshAgent navMeshAgent;
+
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        navMeshAgent.destination = movePositionTransform.position;
+    }
+}
